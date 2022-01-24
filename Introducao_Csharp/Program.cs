284112ead3f67b7f77cf551a07bd94f7c638a4ce
lhgariglios <lhenrique.gsantos@gmail.com>
2022-01-24 16:54:30 -0300

    Primeira parte

---
+++ Introducao_Csharp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System; // Para usar classes do namespace system
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IntroducaoCsharp;
+
+namespace IntroducaoCsharp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Pessoas person = new Pessoas(); // Objeto da classe pessoa.
+			person.Nome = "Luiz";
+			person.Estado = "MG";
+			person.Idade = 18;
+		}
+
+	}
+}
+
+
+/*
+	Namespace: 
+		- Organiza classes;
+		- Podem ter quantos se desejar;
+		- Não pode existir duas classes de mesmo nome em um único namespace;
+		- Nome do namespace Nome da Classe --> Outra forma de usar classes de outros namespaces;
+	Interface:
+		- Classe base que serve de molde para outras;
+*/ 
