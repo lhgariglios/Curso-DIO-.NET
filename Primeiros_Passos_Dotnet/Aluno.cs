9cef49c9481f804263d2a6cb68603bdb6bbc0e75
lhgariglios <lhenrique.gsantos@gmail.com>
2022-01-31 18:41:16 -0300

    Implementando

---
+++ Primeiros_Passos_Dotnet/Aluno.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace PrimeirosPassos
+{
+    public struct Aluno
+    {
+        public string Nome { get; set; }
+        public decimal Nota { get; set; }
+    }
+}
--- Primeiros_Passos_Dotnet/Program.cs
+++ Primeiros_Passos_Dotnet/Program.cs
@@ -7,6 +7,8 @@ namespace PrimeirosPassos
     {
         static void Main(string[] args)
         {
+            Aluno[] alunos = new Aluno[5];
+            var indiceAluno = 0;
             string acao = OpcaoUsuario(); 
 
             while (acao != "4")
@@ -14,8 +16,36 @@ namespace PrimeirosPassos
                 switch(acao)
                 {
                     case "1":
+                        Console.WriteLine();
+                        Console.WriteLine("Informe o nome do aluno: ");
+                        Aluno aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+                        Console.WriteLine("Informe a nota: ");
+
+                        if (decimal.TryParse(Console.ReadLine(), out decimal nota)) //Verifica se é possível a conversão
+                        {
+                            //decimal.TryParse(Console.ReadLine()) --> Transforma a entrada string em decimal
+                            aluno.Nota = nota;
+                        }          
+                        else
+                        {
+                            throw new ArgumentException("Valor da nota deve ser decimal!"); //Reporta erro
+                        }
+
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
+
                         break;
                     case "2":
+                        foreach(var i in alunos) //Para cada
+                        {
+                            if(!string.IsNullOrEmpty(i.Nome))// Se o nome não é nulo ou vazio --> imprimir apenas o preenchido.
+                            {
+                                //$ --> Interpolação
+                                Console.WriteLine($"Aluno: {i.Nome} - Nota: {i.Nota}");
+                            }
+                            
+                        }
                         break;
                     case "3":
                         break;
