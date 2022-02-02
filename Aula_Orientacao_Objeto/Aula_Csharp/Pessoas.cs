02c9cd34db906cc5a7bf24a5f1c0f263dad76754
lhgariglios <lhenrique.gsantos@gmail.com>
2022-02-02 18:19:50 -0300

    Orientação a objeto 1

---
+++ Aula_Orientacao_Objeto/Aula_Csharp/Pessoas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Aula_Orientacao_Objeto
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Ola, meu nome e {Nome} e tenho {Idade} anos.");
+        }
+    }
+}
---
+++ Aula_Orientacao_Objeto/Aula_Csharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Aula_Orientacao_Objeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+            p1.Nome = "Pedro";
+            p1.Idade = 20;
+            p1.Apresentar();
+        }
+    }
+}
