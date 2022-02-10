f37fe5282965273f82484bfabcfb699626bfd3b4
lhgariglios <lhenrique.gsantos@gmail.com>
2022-02-10 15:54:03 -0300

    Heranca

---
+++ Aula_Orientacao_Objeto/Aula_Csharp/Aluno.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_Orientacao_Objeto
+{
+    public class Aluno : Pessoa // Aluno herda a classe Pessoa
+    {
+        public int Nota { get; set; }
+    }
+
+    public class Professor : Pessoa // Professor herda a classe Pessoa
+    {
+        public int Salario { get; set; }
+    }
+}
--- Aula_Orientacao_Objeto/Aula_Csharp/Program.cs
+++ Aula_Orientacao_Objeto/Aula_Csharp/Program.cs
@@ -6,14 +6,23 @@ namespace Aula_Orientacao_Objeto
     {
         static void Main(string[] args)
         {
-            Pessoa p1 = new Pessoa();
+            // Abstração & Herança
+            Aluno a1 = new Aluno();
+            a1.Nome = "Maria";
+            a1.Idade = 17;
+            a1.Nota = 10;
+            a1.Apresentar();
+            Professor p1 = new Professor();
             p1.Nome = "Pedro";
-            p1.Idade = 20;
+            p1.Idade = 25;
+            p1.Salario = 1100;
             p1.Apresentar();
 
+            //Encapsulamento
             Retangulo r = new Retangulo();
             r.DefinirMedidas(30,30);
             System.Console.WriteLine($"Area: {r.ObterArea()}");
+
         }
     }
 }
